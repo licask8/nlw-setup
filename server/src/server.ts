c1e6d2b16a69977486abf9cd269c64b5b7a1b23a
licask8 <elielson.elrick@gmail.com>
2023-01-17 14:25:15 -0300

    iniciando projeto ponta a ponta

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import Fastify from "fastify";
+import {PrismaClient  } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/hello', async () => {
+    const habits = await prisma.habit.findMany({
+        where: {
+            title: {
+                startsWith: 'Beber'
+            }
+        }
+    })
+
+    return habits
+})
+
+
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('Running!')
+})
---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import './styles/global.css'
+import { Habit } from "./components/Habit"
+
+function App() {
+  return (
+   <>
+    <Habit completed={4} />
+    <Habit completed={2} />
+    <Habit completed={1} />
+    <Habit completed={0} />
+   </>
+  )
+}
+
+export default App
